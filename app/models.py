a19b6817121f511049c34edbe40581b92111188f
deezlan <danial.eizlan@yahoo.com>
2026-03-06 12:47:00 +0000

    feat(models0; add User model with hashed password field

--- app/models.py
+++ app/models.py
@@ -1,5 +1,14 @@
-from sqlalchemy import Column, Integer, String, Float, Text
+from sqlalchemy import Column, Integer, String, Float, Text, Boolean
 from app.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
 
 class Game(Base):
     __tablename__ = "games"
